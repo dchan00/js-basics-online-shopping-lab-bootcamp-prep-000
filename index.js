20dcf8c16fe491d5074cc25aeac83ea262fd4a84
DANIEL CHAN <dchan513@msn.com>
2017-09-16 20:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,4 +89,9 @@ function placeOrder(cardNumber) {
   if(cardNumber===undefined){
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
+  else {
+    var total = total()
+    cart.length=0
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`
+  }
 }
