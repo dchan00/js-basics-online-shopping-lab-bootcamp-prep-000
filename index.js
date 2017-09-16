c2e79b19b760ffe469ebfe1b9cc75d912797a3c9
DANIEL CHAN <dchan513@msn.com>
2017-09-16 19:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,15 +23,19 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   var tmp = 'In your cart, you have '
-  for(var i=0; i<cart.length; i++) {
-    if(cart.length===0){
-      console.log('Your shopping cart is empty.')
-    }
-    else if(cart.length===1){
-      tmp += Object.keys(cart[i]) + ' at $' + cart[i][Object.keys(cart[i])] + '.'
-      console.log(tmp)
+  if(cart.length===0){
+    console.log('Your shopping cart is empty.')
+  }
+  else {
+    for(var i=0; i<cart.length; i++) {
+      
+      if(cart.length===1){
+        tmp += Object.keys(cart[i]) + ' at $' + cart[i][Object.keys(cart[i])] + '.'
+        console.log(tmp)
+      }
     }
   }
+  
 }
 
 function total() {
